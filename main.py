8de00175c348e8e0391bc26cfdb49ed3d5f42313
Baishampayan Ghose <b.ghose@ocricket.com>
2009-01-07 20:46:36 +0530

    Misc. changes.

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 ## main.py --  -*- Python -*-
-## Time-stamp: "2009-01-07 19:10:19 ghoseb"
+## Time-stamp: "2009-01-07 20:19:47 ghoseb"
 
 ## Copyright (c) 2009, Baishampayan Ghose <b.ghose@gmail.com>
 ## Copyright (c) 2009, oCricket.com
 
 import os
 import wsgiref.handlers
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    template_values = {}
-    
+    user = users.get_current_user()
+    if user:
+      logout_url = users.create_logout_url('/')
+      login_url = False
+      is_admin = users.is_current_user_admin()
+    else:
+      logout_url = False
+      login_url = users.create_login_url("/")
+      is_admin = False
+
+    template_values = {'user':user,
+                       'login_url':login_url,
+                       'logout_url':logout_url,
+                       'is_admin':is_admin}
+
     path = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
     self.response.out.write(template.render(path, template_values))
 
@@ -28,10 +41,10 @@ class DummyHandler(webapp.RequestHandler):
         """Show the page
         """
         template_values = {}
-        
+
         path = os.path.join(os.path.dirname(__file__), 'templates', 'dummy.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler),
